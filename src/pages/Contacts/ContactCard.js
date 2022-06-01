9da08db4e6de6d1bd030031dea53bbb520f0f4f7
Ensar <ensar.yam54@gmail.com>
2022-06-01 23:47:00 +0300

    son message card

--- src/pages/Contacts/ContactCard.js
+++ src/pages/Contacts/ContactCard.js
@@ -5,18 +5,22 @@ import { Badge } from 'react-native-paper';
 
 const UserCard = ({ item, navigation, messageCount, lastMessage }) => {
     return (
-        <TouchableOpacity onPress={() => navigation.navigate('Chat', {
-            userID: item.userId
-        })} style={styles.cardStyle}>
-            <View style={styles.imageStyle}>
-                <Image style={{ width: '100%', height: '100%', resizeMode: 'cover', borderRadius: 50 }} source={{ uri: 'https://placeimg.com/60/60/any' }} />
-            </View>
-            {/* <Badge size={10} style={styles.badge}></Badge> */}
-            <Text style={{ marginLeft: 10 }}>{item.userName}</Text>
-            <View style={{ flexDirection: 'column' }} >
-                <Text style={{ marginLeft: 10 }}>Son mesaj: {lastMessage}</Text>
-            </View>
-        </TouchableOpacity>
+        <View style={styles.cardStyle}>
+            <TouchableOpacity onPress={() => navigation.navigate('Chat', {
+                userID: item.userId
+            })} style={styles.cardStyle2} >
+                <View style={styles.imageStyle}>
+                    <Image style={{ width: '100%', height: '100%', resizeMode: 'cover', borderRadius: 50 }} source={{ uri: 'https://placeimg.com/60/60/any' }} />
+                </View>
+                {/* <Badge size={10} style={styles.badge}></Badge> */}
+                <Text style={{ marginLeft: 10 }}>{item.userName}</Text>
+            </TouchableOpacity>
+            <Text style={{
+                marginLeft: 60, color: 'grey', fontSize: 12,
+                alignSelf: 'flex-start',
+            }}>{lastMessage}</Text>
+        </View >
+
     )
 }
 
@@ -29,11 +33,23 @@ const styles = StyleSheet.create({
     cardStyle: {
         alignSelf: 'center',
         alignItems: 'center',
-        flexDirection: 'row',
         width: '100%',
         padding: 20,
         marginTop: 10,
         borderRadius: 10,
+        backgroundColor: 'white',
+        shadowColor: "#000",
+        flex: 1,
+        shadowOpacity: 0.2,
+        shadowRadius: 3.00,
+
+        elevation: 0,
+    },
+    cardStyle2: {
+        alignSelf: 'center',
+        alignItems: 'center',
+        flexDirection: 'row',
+        width: '100%',
         backgroundColor: 'white',
         shadowColor: "#000",
         flex: 1,
